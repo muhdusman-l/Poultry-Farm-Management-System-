6cc4296bcf64faee847f156a02fb1a9722d4e93e
Muhammad Usman Lawal <muhammaddanusman@gmail.com>
2024-09-04 14:17:30 +0100

    file assets

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+
+<footer class="footer">
+	<div class="d-sm-flex justify-content-center justify-content-sm-between">
+		<span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2021<a href="https://freeproject24.com" target="_blank">Freeproject24</a>. All rights reserved.</span>
+        <span class="text-muted float-none float-sm-right d-block mt-1 mt-sm-0 text-center pr-3">Presentration by: Freeproject24</span>
+    </div>
+</footer>
